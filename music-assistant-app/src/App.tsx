612c6e55687f0d2dc9aaeb6730fc4ae7bcddac0a
Qneds <bp.panus@gmail.com>
2023-03-08 19:50:14 +0100

    react setup

---
+++ music-assistant-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="card">
+        <button onClick={() => {}}>
+          Click me
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ music-assistant-app/src/commands/commands.ts
@@ -0,0 +1,10 @@
+// tu proponuję umieszczać komendy w liście, będzie lepiej 
+// się obsługować po strone frontu
+
+export const ASSISTANT_COMMANDS = [
+  {
+    commandName: 'jakieś unikalne coś',
+    invokers: ['słowa wywołujące dane komendy, pewnie będzie jedno elementowa taslbica najczęście'],
+    resolver: 'jakaś funkcja',
+  }
+];

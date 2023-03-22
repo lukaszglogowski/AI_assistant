b8c6ea965b3f0f3ce5afd7771bd7a74e7b9bf13d
Qneds <bp.panus@gmail.com>
2023-03-23 00:40:33 +0100

    helper transparency fix

--- music-assistant-app/src/features/CommandHelpDisplayer/CommandHelpDisplayer.tsx
+++ music-assistant-app/src/features/CommandHelpDisplayer/CommandHelpDisplayer.tsx
@@ -11,7 +11,9 @@ const CommandInstance = (props: {data: CommandData}) => {
     <div className={styles['command-instance-container']}>
       <div className={styles['command-instance-container__title']}>{props.data.commandName}</div>
       <div className={styles['command-instance-container__commands']}>
-        Powiedz: {props.data.command.map((el, i) => (<>{(i !== 0) && ', '}<span className={styles.command}>'{el.replace('*', '[szukana fraza]')}'</span></>))}
+        Powiedz: {props.data.command.map((el, i) => (
+          <React.Fragment key={el}>{(i !== 0) && ', '}<span className={styles.command}>'{el.replace('*', '[szukana fraza]')}'</span></React.Fragment>
+        ))}
       </div>
       <div className={styles['command-instance-container__description']}>{props.data.description}</div>
     </div>
@@ -32,10 +34,10 @@ export const CommandHelpDisplayer = (props: CommandHelpDisplayerProps) => {
       <span className={styles['command-help-conatiner__title']}>Dostępne komendy głosowe</span>
         <div>
               {commands.current.map((item) => (
-                <>
+                <React.Fragment key={item.commandId}>
                   <Divider/>
                   <CommandInstance data={item} />
-                </>
+                </React.Fragment>
               ))}
         </div>
     </div>

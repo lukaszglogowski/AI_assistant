a42d214b776f79e4515e9904171372c39a4b9811
Qneds <bp.panus@gmail.com>
2023-05-10 16:38:40 +0200

    album, artist, search endpoints

--- music-assistant-app/src/utils/apis/shazam.ts
+++ music-assistant-app/src/utils/apis/shazam.ts
@@ -1,26 +1,16 @@
 import { getEnv } from 'config';
 import createEndpoint, { ApiConfigs } from 'utils/fetch';
-import { ShazamDetectSongRequestBody, ShazamDetectSongResponseBody, ShazamDetectSongSearchParams, ShazamInfoKeysRequestBody, ShazamInfoKeysResponseBody, ShazamInfoKeysSearchParams, ShazamSongInfoRequestBody, ShazamSongInfoResponseBody, ShazamSongInfoSearchParams } from './shazam.types';
+import { ShazamAlbumInfoRequestBody, ShazamAlbumInfoResponseBody, ShazamAlbumInfoSearchParams, ShazamArtistInfoRequestBody, ShazamArtistInfoResponseBody, ShazamArtistInfoSearchParams, ShazamArtistSummaryInfoRequestBody, ShazamArtistSummaryInfoResponseBody, ShazamArtistSummaryInfoSearchParams, ShazamDetectSongRequestBody, ShazamDetectSongResponseBody, ShazamDetectSongSearchParams, ShazamInfoKeysRequestBody, ShazamInfoKeysResponseBody, ShazamInfoKeysSearchParams, ShazamSearchRequestBody, ShazamSearchResponseBody, ShazamSearchSearchParams, ShazamSongInfoRequestBody, ShazamSongInfoResponseBody, ShazamSongInfoSearchParams } from './shazam.types';
 
 const BASE_URL = getEnv('SHAZAM_API_BASE_URL')
-
-type URLParameters = {
-  autohorId: string;
-}
-
-type URLParams = {
-  filter: string;
-}
-
-type ResponseBody = {
-  status: string
-}
-
 
 export const SHAZAM_API = {
   autors: {
     details: {
-      GET: createEndpoint<URLParameters, URLParams, string, ResponseBody>('GET', (parameters) => `${BASE_URL}/author/${parameters.autohorId}/details`)
+      GET: createEndpoint<{}, ShazamArtistInfoSearchParams, ShazamArtistInfoRequestBody, ShazamArtistInfoResponseBody>('GET', () => `${BASE_URL}/artists/get-details`)
+    },
+    summary: {
+      GET: createEndpoint<{}, ShazamArtistSummaryInfoSearchParams, ShazamArtistSummaryInfoRequestBody, ShazamArtistSummaryInfoResponseBody>('GET', () => `${BASE_URL}/artists/get-summary`)
     }
   },
   songs: {
@@ -28,8 +18,16 @@ export const SHAZAM_API = {
       POST: createEndpoint<{}, ShazamDetectSongSearchParams, ShazamDetectSongRequestBody, ShazamDetectSongResponseBody>('POST', () => `${BASE_URL}/songs/v2/detect`)
     },
     details: {
-      GET:createEndpoint<{}, ShazamSongInfoSearchParams, ShazamSongInfoRequestBody, ShazamSongInfoResponseBody>('GET', () => `${BASE_URL}/songs/v2/get-details`)
+      GET: createEndpoint<{}, ShazamSongInfoSearchParams, ShazamSongInfoRequestBody, ShazamSongInfoResponseBody>('GET', () => `${BASE_URL}/songs/v2/get-details`)
     },
+  },
+  search: {
+    GET: createEndpoint<{}, ShazamSearchSearchParams, ShazamSearchRequestBody, ShazamSearchResponseBody>('GET', () => `${BASE_URL}/search`)
+  },
+  albums: {
+    details: {
+      GET: createEndpoint<{}, ShazamAlbumInfoSearchParams, ShazamAlbumInfoRequestBody, ShazamAlbumInfoResponseBody>('GET', () => `${BASE_URL}/albums/get-details`)
+    }
   },
   infoKeys: {
     GET: createEndpoint<{}, ShazamInfoKeysSearchParams, ShazamInfoKeysRequestBody, ShazamInfoKeysResponseBody>('GET', () => `${BASE_URL}/shazam-songs/get-details`)
--- music-assistant-app/src/utils/apis/shazam.types.ts
+++ music-assistant-app/src/utils/apis/shazam.types.ts
@@ -27,6 +27,128 @@ type GenericIdSearchParams = {
   id: string;
 };
 
+type ShareObject = {
+  href?: URLString;
+  avatar?: URLString;
+  subject: string;
+  text: string;
+  image: URLString;
+  twitter: string
+  html: URLString;
+  snapchat: URLString;
+};
+
+type ArtworkObject = {
+  width: number;
+  url: URLString;
+  height: number;
+  textColor3: string;
+  textColor2: string;
+  textColor4: string;
+  textColor1: string;
+  bgColor: string;
+  hasP3: boolean;
+}
+
+type ImagesObject = {
+  artistAvatar: URLString;
+  coverArt: URLString;
+  coverArtHq: URLString;
+  joecolor?: string;
+};
+
+
+type ArtistSearchObject = {
+  avatar: URLString;
+  id: string;
+  name: string;
+  verified: boolean;
+  weburl: URLString;
+  adamid: string;
+}
+
+type SongSearchObject = {
+  layout: string;
+  type: string;
+  key: string;
+  title: string;
+  subtitle: string;
+  share: ShareObject;
+  images: ImagesObject;
+  artists: {
+    id: string;
+    adamid: string;
+  }[]
+  url: URLString;
+}
+
+
+
+type SongAttributes = {
+  albumName: string;
+  hasTimeSyncedLyrics: boolean;
+  genreNames: string[];
+  trackNumber: number;
+  releaseDate: string;
+  durationInMillis: number;
+  isVocalAttenuationAllowed: boolean,
+  isMasteredForItunes: boolean,
+  isrc: string;
+  artwork: ArtworkObject;
+  audioLocale: string;
+  composerName: string;
+  playParams: {
+    id: string;
+    kind: string;
+  };
+  url: URLString;
+  discNumber: number;
+  hasLyrics: boolean;
+  isAppleDigitalMaster: boolean;
+  audioTraits: string[];
+  name: string;
+  previews: {
+    url: URLString;
+  }[];
+  artistName: string;
+}
+
+
+type ArtistAttributes = {
+  genreNames: string[];
+  name: string;
+  artwork: ArtworkObject;
+  editorialNotes: {
+    name: string;
+    standard: string;
+    short: string;
+  };
+  url?: URLString;
+}
+
+type AlbumAttributes = {
+  copyright: string;
+  genreNames: string[];
+  releaseDate: string; 
+  isMasteredForItunes: boolean;
+  upc: string;
+  artwork: ArtworkObject;
+  url: URLString;
+  playParams: {
+    id: string;
+    kind: string;
+  };
+  recordLabel: string;
+  isCompilation: boolean;
+  trackCount: number;
+  isPrerelease: string;
+  audioTraits: string[];
+  isSingle: boolean;
+  name: string;
+  artistName: string;
+  isComplete: boolean;
+}
+
 
 
 export type ShazamDetectSongSearchParams = {
@@ -40,9 +162,7 @@ export type ShazamDetectSongSearchParams = {
 export type ShazamDetectSongRequestBody = string;
 
 export type ShazamDetectSongResponseBody = {
-  track: {
-    key: string;
-  }
+  track: SongSearchObject;
 };
 
 
@@ -102,19 +222,8 @@ export type ShazamInfoKeysResponseBody = {
           explicit: boolean;
           isrc: string;
           webUrl: URLString;
-          images: {
-            artistAvatar: URLString;
-            coverArt: URLString;
-            coverArtHq: URLString;
-          };
-          share: {
-            subject: string;
-            text: string;
-            image: URLString;
-            twitter: string
-            html: URLString;
-            snapchat: URLString;
-          };
+          images: ImagesObject;
+          share: ShareObject;
           genres: {
             primary: string
           };
@@ -144,44 +253,152 @@ export type ShazamSongInfoResponseBody = {
   data: {
     id: string;
     type: string;
-    attributes: {
-      albumName: string;
-      hasTimeSyncedLyrics: boolean;
-      genreNames: string[];
-      trackNumber: number;
-      releaseDate: string;
-      durationInMillis: number;
-      isVocalAttenuationAllowed: boolean,
-      isMasteredForItunes: boolean,
-      isrc: string;
-      artwork: {
-        width: number;
-        url: URLString;
-        height: number;
-        textColor3: string;
-        textColor2: string;
-        textColor4: string;
-        textColor1: string;
-        bgColor: string;
-        hasP3: false
-      },
-      audioLocale: string;
-      composerName: string;
-      playParams: {
-        id: string;
-        kind: string;
-      },
-      url: URLString;
-      discNumber: number;
-      hasLyrics: boolean;
-      isAppleDigitalMaster: boolean;
-      audioTraits: string[];
-      name: string;
-      previews: {
-        url: URLString;
-      }[];
-      artistName: string;
-    };
+    attributes: SongAttributes;
     relationships: RelationShipsType<'songs'>;
   }[]
 };
+
+
+
+
+
+export type ShazamSearchSearchParams = {
+  locale?: string;
+  offset?: string;
+  limit?: string;
+  term: string;
+}
+
+export type ShazamSearchRequestBody = null;
+
+export type ShazamSearchResponseBody = {
+  tracks: {
+    hits: {
+      track: SongSearchObject;
+      snippet: string;
+    }[]
+  };
+  artists: {
+    hits: {
+      artist: ArtistSearchObject
+    }[]
+  };
+}
+
+
+
+
+
+
+
+export type ShazamArtistInfoSearchParams = GenericIdSearchParams & {
+  l?: string;
+}
+
+export type ShazamArtistInfoRequestBody = null;
+
+export type ShazamArtistInfoResponseBody = {
+  data: {
+    id: string;
+    type: string;
+    attributes: ArtistAttributes;
+    relationships: {
+      albums: {
+        data: {
+          id: string;
+          type: string;
+        }[];
+      };
+    };
+  }[]
+}
+
+
+export type ShazamArtistSummaryInfoSearchParams = GenericIdSearchParams & {
+  l?: string;
+}
+
+export type ShazamArtistSummaryInfoRequestBody = null;
+
+export type ShazamArtistSummaryInfoResponseBody = {
+  data: {
+    id: string;
+    type: string;
+  }[]
+  resources: {
+    artists: {
+      [keyId: string]: {
+        id: string;
+        type: string;
+        attributes: ArtistAttributes;
+        relationships: {
+          albums: {
+            data: {
+              id: string;
+              type: string;
+            }[];
+          };
+        };
+        views: {
+          'latest-release': {
+            attributes: {title: string};
+            data: {id: string, type: string}[];
+          }
+          'tops-songs': {
+            attributes: {title: string};
+            data: {id: string, type: string}[];
+          }
+        }
+        meta: {
+          views: {
+            order: string[];
+          }
+        }
+      }
+    }
+    songs: {
+      [keyId: string]: {
+        id: string;
+        type: string;
+        attributes: SongAttributes;
+      }
+    }
+    albums: {
+      [keyId: string]: {
+        id: string;
+        type: string;
+        attributes: AlbumAttributes;
+      }
+    }
+  }
+}
+
+
+export type ShazamAlbumInfoSearchParams = GenericIdSearchParams & {
+  l?: string;
+}
+
+export type ShazamAlbumInfoRequestBody = null;
+
+export type ShazamAlbumInfoResponseBody = {
+  data: {
+    id: string;
+    type: string;
+    attributes: AlbumAttributes;
+    relationships: {
+      artists: {
+        data: {
+          id: string;
+          type: string;
+        }[];
+      };
+      tracks: {
+        data: {
+          id: string;
+          type: string;
+          attributes: SongAttributes;
+        }[];
+      };
+    };
+  }[];
+}
